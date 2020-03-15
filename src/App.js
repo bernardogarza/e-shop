3c3ed7ffca09f7f94cf1130ab2b22dfffb807b80
bernardogarza <bernardo.g.landa@gmail.com>
2020-03-15 13:02:55 -0600

    Impletemented firebase email and password auth

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
+          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
