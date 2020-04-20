f5d7382576716b96873d2afe7c1f9c9d6f0bc239
bernardogarza <bernardo.g.landa@gmail.com>
2020-04-20 18:21:47 -0500

    moved our directory and shop data into it's own reducer, created respective selector and updated to respoding component with new ux flow for directory/shop

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import { Route } from 'react-router-dom';
 
-const ShopPage = ({ collections }) => (
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
